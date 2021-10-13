fd35afae9cb40e43674819fcb5feda9ad974f00a
matibesme <83069764+matibesme@users.noreply.github.com>
2021-10-13 09:01:15 -0300

    anasheiiiiiiiii

--- Tp9/app/src/main/java/com/example/tp9/MainActivity.java
+++ Tp9/app/src/main/java/com/example/tp9/MainActivity.java
@@ -2,13 +2,79 @@ package com.example.tp9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button boton;
+    ImageButton botonGaleria;
+    private static final int SECOND_ACTIVITY_REQUEST_CODE = 100;
+    private static final int CAMERA_REQUEST = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ObtenerReferencias();
+
+        SetearListners();
+    }
+
+    private void SetearListners() {
+        boton.setOnClickListener(boton_click);
+        botonGaleria.setOnClickListener(botonGaleria_click);
+    }
+
+    View.OnClickListener boton_click= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Intent intent = new Intent(getBaseContext(), SecondActivity.class);
+            startActivityForResult(intent, SECOND_ACTIVITY_REQUEST_CODE);
+        }
+    };
+
+    View.OnClickListener botonGaleria_click= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        }
+    };
+
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+            if (resultCode == RESULT_OK) {
+                if (requestCode == SECOND_ACTIVITY_REQUEST_CODE) {
+                String returnString = data.getStringExtra("keyName");
+
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText(returnString);
+                textView.setVisibility(View.VISIBLE);
+            }
+
+                if (requestCode == CAMERA_REQUEST ){
+                    Bitmap photo = (Bitmap) data.getExtras().get("data");
+                    botonGaleria.setImageBitmap(photo);
+                }
+        }
+    }
+
+    public void ObtenerReferencias(){
+        boton= (Button) findViewById(R.id.button);
+        botonGaleria= (ImageButton) findViewById(R.id.botonGaleria);
     }
 }
---
+++ Tp9/app/src/main/java/com/example/tp9/SecondActivity.java
@@ -0,0 +1,54 @@
+package com.example.tp9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+    Button boton2;
+    ImageButton botonGaleria;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        ObtenerReferencias();
+
+        SetearListners();
+    }
+
+
+    private void SetearListners() {
+        boton2.setOnClickListener(boton2_click);
+        botonGaleria.setOnClickListener(boton2_click);
+    }
+
+    View.OnClickListener boton2_click= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            EditText editText = (EditText) findViewById(R.id.editText);
+            String stringToPassBack = editText.getText().toString();
+
+            // Put the String to pass back into an Intent and close this activity
+            Intent intent = new Intent();
+            intent.putExtra("keyName", stringToPassBack);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    };
+
+    public void ObtenerReferencias(){
+        boton2= (Button) findViewById(R.id.button2);
+        botonGaleria= (ImageButton) findViewById(R.id.botonGaleria);
+    }
+
+
+}
